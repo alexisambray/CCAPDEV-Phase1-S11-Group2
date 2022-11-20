57c6f2ae5712ce62bbc1586f63c07ebca1818277
elliamaeang <elliamae_ang@dlsu.edu.ph>
2022-11-21 00:14:14 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const bodyParser = require("body-parser");
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 //URL Routing
 
+/* index.ejs */
+app.get("/", function(req, res){
+    res.render("index");
+});
 
 //Listener
 app.listen(3000, function() {
