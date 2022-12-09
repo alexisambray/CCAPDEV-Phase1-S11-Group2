dea79bcf411b4193cf35d417f570f6acd30a7924
elliamaeang <elliamae_ang@dlsu.edu.ph>
2022-12-09 19:02:23 +0800

    Made table names small caps for case sensitivity

--- model/db.sql
+++ model/db.sql
@@ -3,14 +3,14 @@ UNLOCK TABLES;
 CREATE DATABASE IF NOT EXISTS `mpdb`;
 USE `mpdb`;
 
-DROP TABLE IF EXISTS `Likes`;
-DROP TABLE IF EXISTS `Comments`;
-DROP TABLE IF EXISTS `User_Bookmarks`;
-DROP TABLE IF EXISTS `User_Posts`;
-DROP TABLE IF EXISTS `Users`;
+DROP TABLE IF EXISTS `likes`;
+DROP TABLE IF EXISTS `comments`;
+DROP TABLE IF EXISTS `user_bookmarks`;
+DROP TABLE IF EXISTS `user_posts`;
+DROP TABLE IF EXISTS `users`;
 
 -- users table ------
-CREATE TABLE `Users` (
+CREATE TABLE `users` (
   `Username` varchar(20) NOT NULL,
   `Password` varchar(60) NOT NULL,
   `Email` varchar(50) NOT NULL, 
@@ -21,21 +21,21 @@ CREATE TABLE `Users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 /*Note: All sample passwords are "password" in md5 hash*/
-INSERT INTO `Users` VALUES ('brylil','$2a$10$J8G9AUA6MAOBa4292wCI0OHV5hXAt5vUHZZLLeloyIBPZK/u2Y9aq','brylil@gmail.com','/images/icons/user3.jpeg', 'Bryan', 'Asian traveller'),
+INSERT INTO `users` VALUES ('brylil','$2a$10$J8G9AUA6MAOBa4292wCI0OHV5hXAt5vUHZZLLeloyIBPZK/u2Y9aq','brylil@gmail.com','/images/icons/user3.jpeg', 'Bryan', 'Asian traveller'),
 ('elliamae','$2a$10$J8G9AUA6MAOBa4292wCI0OHV5hXAt5vUHZZLLeloyIBPZK/u2Y9aq','ellia@yahoo.com','/images/icons/user1.jpg','Ellia','Hello world! This is my bio'),
 ('emman','$2a$10$J8G9AUA6MAOBa4292wCI0OHV5hXAt5vUHZZLLeloyIBPZK/u2Y9aq','emman@gmail.com','/images/icons/user4.jpg','Emman','Bon voyage!'),
 ('alexis','$2a$10$J8G9AUA6MAOBa4292wCI0OHV5hXAt5vUHZZLLeloyIBPZK/u2Y9aq','alex23@hotmail.com','/images/icons/user2.jpeg','Alexis','I like photography'),
 ('ccapdev','$2a$10$J8G9AUA6MAOBa4292wCI0OHV5hXAt5vUHZZLLeloyIBPZK/u2Y9aq','apdev@gmail.com','/images/icons/user5.jpg','Apdev','This is MP Phase 2.');
 
-LOCK TABLES `Users` WRITE;
+LOCK TABLES `users` WRITE;
 
 -- user_posts table ------
 UNLOCK TABLES;
-CREATE TABLE `User_Posts` (
+CREATE TABLE `user_posts` (
   `PostID` int NOT NULL AUTO_INCREMENT,   
   `Title` varchar(30) NOT NULL,  
   `Username` varchar(20) NOT NULL,   
-  `Photo` varchar(100) NOT NULL,   
+  `Photo` varchar(100) NOT NULL,
   `Date` DATE NOT NULL,
   `Tags` varchar(50) DEFAULT NULL,
   `Caption` varchar(280) DEFAULT NULL,   
@@ -45,17 +45,17 @@ CREATE TABLE `User_Posts` (
   PRIMARY KEY (`PostID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-ALTER TABLE User_Posts
-	ADD FOREIGN KEY (Username) REFERENCES Users (Username);
+ALTER TABLE user_posts
+	ADD FOREIGN KEY (Username) REFERENCES users (Username);
 
-INSERT INTO `User_Posts` (`Title`, `Username`, `Photo`, `Date`, `Tags`, `Caption`, `LikeCount`, `BookmarkCount`, `CommentCount`)
+INSERT INTO `user_posts` (`Title`, `Username`, `Photo`, `Date`, `Tags`, `Caption`, `LikeCount`, `BookmarkCount`, `CommentCount`)
 VALUES ('Maldives','brylil','/images/posts/myPost1.jpg','2022-11-23','travel, photography, beach','Picture I took!','1','2','1'),
 ('Philippines','elliamae','/images/posts/myPost2.jpg','2022-11-23','photography, beach','Look at this place!','1','2','2'),
 ('Sweden','emman','/images/posts/myPost3.jpg','2022-11-23','travel','What a dazzling view','1','1','1'),
 ('NYC','alexis','/images/posts/myPost4.jpeg','2022-11-23','photo, travel, city','New York City','1','0','0'),
 ('Toronto','ccapdev','/images/posts/myPost5.jpeg','2022-11-23','city','I am now in Canada wow','1','0','1');
 
-LOCK TABLES `User_Posts` WRITE;
+LOCK TABLES `user_posts` WRITE;
 
 -- Testing Purposes
 -- SELECT * FROM User_Posts;
@@ -65,12 +65,12 @@ LOCK TABLES `User_Posts` WRITE;
 
 -- user_bookmarks table ------
 UNLOCK TABLES;
-CREATE TABLE `User_Bookmarks` (
+CREATE TABLE `user_bookmarks` (
   `Username` varchar(20) NOT NULL,
   `PostID` int NOT NULL,   
   PRIMARY KEY (`Username`, `PostID`),
-  FOREIGN KEY (`Username`) REFERENCES Users (`Username`),
-  FOREIGN KEY (`PostID`) REFERENCES User_Posts (`PostID`)
+  FOREIGN KEY (`Username`) REFERENCES users (`Username`),
+  FOREIGN KEY (`PostID`) REFERENCES user_posts (`PostID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- UNLOCK TABLES;
@@ -78,13 +78,13 @@ CREATE TABLE `User_Bookmarks` (
 	ADD FOREIGN KEY (Username) REFERENCES Users (Username),
   ADD FOREIGN KEY (PostID) REFERENCES User_Posts (PostID);*/
 
-INSERT INTO `User_Bookmarks` VALUES ('brylil',2),
+INSERT INTO `user_bookmarks` VALUES ('brylil',2),
 ('elliamae',1),
 ('emman',3),
 ('alexis',2),
 ('alexis',1);
 
-LOCK TABLES `User_Bookmarks` WRITE;
+LOCK TABLES `user_bookmarks` WRITE;
 
 
 -- Testing Purposes
@@ -96,7 +96,7 @@ LOCK TABLES `User_Bookmarks` WRITE;
 
 -- comments table ------
 UNLOCK TABLES;
-CREATE TABLE `Comments` (
+CREATE TABLE `comments` (
   `CommentID` int NOT NULL AUTO_INCREMENT,   
   `PostID` int NOT NULL,  
   `Username` varchar(20) NOT NULL,   
@@ -105,13 +105,13 @@ CREATE TABLE `Comments` (
   PRIMARY KEY (`CommentID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-ALTER TABLE Comments
-	ADD FOREIGN KEY (Username) REFERENCES Users (Username),
-  ADD FOREIGN KEY (PostID) REFERENCES User_Posts(PostID);
+ALTER TABLE comments
+	ADD FOREIGN KEY (Username) REFERENCES users (Username),
+  ADD FOREIGN KEY (PostID) REFERENCES user_posts(PostID);
 
-LOCK TABLES `Comments` WRITE;
+LOCK TABLES `comments` WRITE;
 
-INSERT INTO `Comments` (`PostID`, `Username`, `Comment`, `Date`) 
+INSERT INTO `comments` (`PostID`, `Username`, `Comment`, `Date`) 
 VALUES (1,'emman','This looks good!','2022-11-23'),
 (2,'alexis','go girl!','2022-11-23'),
 (3,'elliamae','What a nice photo','2022-11-23'),
@@ -126,12 +126,12 @@ VALUES (1,'emman','This looks good!','2022-11-23'),
 
 -- likes table ------
 UNLOCK TABLES;
-CREATE TABLE `Likes` (
+CREATE TABLE `likes` (
   `Username` varchar(20) NOT NULL,
   `PostID` int NOT NULL,   
   PRIMARY KEY (`Username`, `PostID`),
-  FOREIGN KEY (`Username`) REFERENCES Users (`Username`),
-  FOREIGN KEY (`PostID`) REFERENCES User_Posts (`PostID`)
+  FOREIGN KEY (`Username`) REFERENCES users (`Username`),
+  FOREIGN KEY (`PostID`) REFERENCES user_posts (`PostID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- UNLOCK TABLES;
@@ -139,13 +139,13 @@ CREATE TABLE `Likes` (
 	ADD FOREIGN KEY (Username) REFERENCES Users (Username),
   ADD FOREIGN KEY (PostID) REFERENCES User_Posts (PostID);*/
 
-INSERT INTO `Likes` VALUES ('brylil',5),
+INSERT INTO `likes` VALUES ('brylil',5),
 ('elliamae',4),
 ('emman',3),
 ('alexis',2),
 ('alexis',1);
 
-LOCK TABLES `User_Bookmarks` WRITE;
+LOCK TABLES `user_bookmarks` WRITE;
 
 
 -- User_Notifications Table --
